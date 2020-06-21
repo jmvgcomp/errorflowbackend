b09a67357f4cae138da0498d0077fef93c1f73c2
Joanantha Matheus <jmvg.comp@gmail.com>
2020-06-21 00:52:31 -0300

    Commit inicial

---
+++ src/main/java/dev/jmvg/codenation/errorflow/api/ErrorflowApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jmvg.codenation.errorflow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorflowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jmvg/codenation/errorflow/api/ErrorflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.codenation.errorflow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorflowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
