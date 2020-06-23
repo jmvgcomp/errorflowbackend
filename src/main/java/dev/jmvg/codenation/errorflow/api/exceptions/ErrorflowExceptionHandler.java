2d8cf72effba7e5b2584928008618cb8468f4080
Joanantha Matheus <jmvg.comp@gmail.com>
2020-06-23 03:27:13 -0300

    Exception handler

--- src/main/java/dev/jmvg/codenation/errorflow/api/exceptions/ErrorflowExceptionHandler.java
+++ src/main/java/dev/jmvg/codenation/errorflow/api/exceptions/ErrorflowExceptionHandler.java
@@ -1,5 +1,11 @@
 package dev.jmvg.codenation.errorflow.api.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +14,34 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Locale;
+
 @ControllerAdvice
 public class ErrorflowExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+    }
+
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        return handleExceptionInternal(ex, "Mensagem inválida", headers, HttpStatus.BAD_REQUEST, request );
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String userMessage = messageSource().getMessage("invalid.message", null, LocaleContextHolder.getLocale());
+        String developerMessage = ex.getCause().toString();
+        return handleExceptionInternal(ex, new Erro(userMessage, developerMessage), headers, HttpStatus.BAD_REQUEST, request );
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Erro{
+        private String userMessage;
+        private String developerMessage;
     }
 }
